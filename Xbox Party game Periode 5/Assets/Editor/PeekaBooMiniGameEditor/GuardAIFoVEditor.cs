99ef7600d96f4ac66a009cb39e12f1eed23a89c7
karlo <k.koelewijn2@gmail.com>
2019-10-08 11:49:14 +0200

    guard FoV
    not finished but working for now, gotta finish it after pulling it

---
+++ Xbox Party game Periode 5/Assets/Editor/PeekaBooMiniGameEditor/GuardAIFoVEditor.cs
@@ -0,0 +1,21 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEditor;
+
+[ExecuteInEditMode, CustomEditor(typeof(GuardAI))]
+public class GuardAIFoVEditor : Editor
+{
+    private void OnSceneGUI()
+    {
+        GuardAI FoV = (GuardAI)target;
+        Handles.color = Color.black;
+        Handles.DrawWireArc(FoV.transform.position, Vector3.up, Vector3.forward, 360f, FoV.m_viewRadius);
+
+        Vector3 viewAngleA = FoV.DirFromAngle(-FoV.m_viewAngle / 2, false);
+        Vector3 viewAngleB = FoV.DirFromAngle(FoV.m_viewAngle / 2, false);
+
+        Handles.DrawLine(FoV.transform.position, FoV.transform.position + viewAngleA * FoV.m_viewRadius);
+        Handles.DrawLine(FoV.transform.position, FoV.transform.position + viewAngleB * FoV.m_viewRadius);
+    }
+}
