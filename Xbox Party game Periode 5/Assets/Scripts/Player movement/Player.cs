adc4553afda6c4ba7d4d225a7a9ab4b05d12a962
Bouke <BoukeWeel@outlook.com>
2019-10-08 12:43:16 +0200

    eind les

--- Xbox Party game Periode 5/Assets/Scripts/Player movement/Player.cs
+++ Xbox Party game Periode 5/Assets/Scripts/Player movement/Player.cs
@@ -3,11 +3,12 @@ using System.Collections.Generic;
 using UnityEngine;
 using XboxCtrlrInput;
 
-public class PlayerMovement : MonoBehaviour
+public class Player : MonoBehaviour
 {
     //speeds
     public Vector3 M_Speed;
-    public float Speed;
+    public float Speed = 5;
+    private float holdspeed;
 
     [Range(0,1)]
     public float M_deadzone;
@@ -17,9 +18,12 @@ public class PlayerMovement : MonoBehaviour
     private float _YAxis;
     private Rigidbody rig;
 
+    
+
     private void Start()
     {
         rig = GetComponent<Rigidbody>();
+        holdspeed = Speed;
     }
 
 
@@ -42,5 +46,15 @@ public class PlayerMovement : MonoBehaviour
 
         rig.MovePosition(transform.position + (M_Speed * Speed) * Time.deltaTime);
 
+        if(XCI.GetButtonDown(XboxButton.A, Player_Nummber))
+        {
+            gameObject.transform.localScale = new Vector3(0.012f, 0.007f, 0.01f);
+            Speed = 2;
+        }
+        if(XCI.GetButtonUp(XboxButton.A, Player_Nummber))
+        {
+            gameObject.transform.localScale = new Vector3(0.01f, 0.01f, 0.01f);
+            Speed = holdspeed;
+        }
     }
 }
