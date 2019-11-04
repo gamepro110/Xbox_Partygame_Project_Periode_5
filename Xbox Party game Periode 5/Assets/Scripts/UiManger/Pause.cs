892391da4e30de2f17ba4fadc7e24226fc49507a
Bouke <BoukeWeel@outlook.com>
2019-11-04 19:10:02 +0100

    Auto stash before merge of "Bouke" and "origin/Bouke"

---
+++ Xbox Party game Periode 5/Assets/Scripts/UiManger/Pause.cs
@@ -0,0 +1,55 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using XboxCtrlrInput;
+
+public class Pause : MonoBehaviour
+{
+    public GameObject Pausemenu;
+    public GameObject AreYouSure;
+    private bool IsPaused = false;
+
+    private void Start()
+    {
+        Pausemenu.SetActive(false);
+        AreYouSure.SetActive(false);
+
+    }
+    private void Update()
+    {
+        if (XCI.GetButtonUp(XboxButton.Start, XboxController.Any))
+        {
+            IsPaused = !IsPaused;
+        }
+        if(IsPaused == true)
+        {
+            Pausemenu.SetActive(true);
+            Time.timeScale = 0;
+        }
+        if(IsPaused == false)
+        {
+            Pausemenu.SetActive(false);
+            Time.timeScale = 1;
+        }
+    }
+    public void QuitAreYouSure()
+    {
+        Pausemenu.SetActive(false);
+        AreYouSure.SetActive(true);
+    }
+    public void PauseMenu()
+    {
+        Pausemenu.SetActive(true);
+        AreYouSure.SetActive(false);
+    }
+    public void UnPause()
+    {
+        Pausemenu.SetActive(false);
+        AreYouSure.SetActive(false);
+        Time.timeScale = 1;
+    }
+    public void Quit()
+    {
+        Application.Quit();
+    }
+}
--- Xbox Party game Periode 5/Assets/Scripts/UiManger/UiManger.cs
+++ Xbox Party game Periode 5/Assets/Scripts/UiManger/UiManger.cs
@@ -7,20 +7,75 @@ public class UiManger : MonoBehaviour
 {
     public GameObject mainMenuobj;
     public GameObject credits;
-    public Canvas gameCanvas;
-    public Canvas mainMenucan;
+    //public Canvas gameCanvas;
+    //public Canvas mainMenucan;
     public GameObject youSure;
     public GameObject howManyPlayer;
+    public GameObject CanvasGame;
+    public GameObject Finishobj;
 
     private void Start()
     {
+        Finishobj.SetActive(false);
         mainMenuobj.SetActive(true);
         credits.SetActive(false);
         youSure.SetActive(false);
         howManyPlayer.SetActive(false);
+        CanvasGame.SetActive(false);
     }
     public void MainMenuObj()
     {
-
+        Finishobj.SetActive(false);
+        mainMenuobj.SetActive(true);
+        credits.SetActive(false);
+        youSure.SetActive(false);
+        howManyPlayer.SetActive(false);
+        CanvasGame.SetActive(false);
+    }
+    public void HowManyPlayer_()
+    {
+        Finishobj.SetActive(false);
+        howManyPlayer.SetActive(true);
+        mainMenuobj.SetActive(false);
+        credits.SetActive(false);
+        youSure.SetActive(false);
+    }
+    public void Credits_()
+    {
+        Finishobj.SetActive(false);
+        howManyPlayer.SetActive(false);
+        mainMenuobj.SetActive(false);
+        credits.SetActive(true);
+        youSure.SetActive(false);
+    }
+    public void YouSure()
+    {
+        Finishobj.SetActive(false);
+        howManyPlayer.SetActive(false);
+        mainMenuobj.SetActive(false);
+        credits.SetActive(false);
+        youSure.SetActive(true);
+    }
+    public void Quit()
+    {
+        Application.Quit();
+    }
+    public void GameStart()
+    {
+        Finishobj.SetActive(false);
+        CanvasGame.SetActive(true);
+        howManyPlayer.SetActive(false);
+        mainMenuobj.SetActive(false);
+        credits.SetActive(false);
+        youSure.SetActive(false);
+    }
+    public void EndScene()
+    {
+        CanvasGame.SetActive(false);
+        howManyPlayer.SetActive(false);
+        mainMenuobj.SetActive(false);
+        credits.SetActive(false);
+        youSure.SetActive(false);
+        Finishobj.SetActive(true);
     }
 }
